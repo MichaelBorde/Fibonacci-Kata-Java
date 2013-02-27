7f32f20f3282057158ed8309c94746885803ccb2
Michael Borde <michael.borde@gmail.com>
2013-02-27 23:24:16 +0100

    refacto

---
+++ src/main/java/FibonacciJava/Fibonacci.java
@@ -0,0 +1,15 @@
+package FibonacciJava;
+
+public class Fibonacci {
+
+	public int pour(int nombre) {
+		return fonctionAuxiliaire(nombre, 0, 1);
+	}
+
+	private static int fonctionAuxiliaire(int nombre, int résultat, int résultatSuivant) {
+		if (nombre == 0) {
+			return résultat;
+		}
+		return fonctionAuxiliaire(nombre - 1, résultatSuivant, résultat + résultatSuivant);
+	}
+}
--- src/test/java/FibonacciJava/TestFibonacci.java
+++ src/test/java/FibonacciJava/TestFibonacci.java
@@ -2,44 +2,38 @@ package FibonacciJava;
 
 import static org.fest.assertions.Assertions.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestFibonacci {
 
+	private Fibonacci fibonacci;
+
+	@Before
+	public void avantChaqueTest() {
+		fibonacci = new Fibonacci();
+	}
+
 	@Test
 	public void fiboDe0Donne0() {
-		assertThat(fibo(0)).isEqualTo(0);
+		assertThat(fibonacci.pour(0)).isEqualTo(0);
 	}
 
 	@Test
 	public void fiboDe1Donne1() {
-		assertThat(fibo(1)).isEqualTo(1);
+		assertThat(fibonacci.pour(1)).isEqualTo(1);
 	}
 
 	@Test
 	public void peutCalculerLesRésultatsGénéraux() {
-		assertThat(fibo(2)).isEqualTo(1);
-		assertThat(fibo(3)).isEqualTo(2);
-		assertThat(fibo(4)).isEqualTo(3);
-		assertThat(fibo(8)).isEqualTo(21);
+		assertThat(fibonacci.pour(2)).isEqualTo(1);
+		assertThat(fibonacci.pour(3)).isEqualTo(2);
+		assertThat(fibonacci.pour(4)).isEqualTo(3);
+		assertThat(fibonacci.pour(8)).isEqualTo(21);
 	}
 
 	@Test
 	public void ilEstPossibleDeConnaitreLeRésultatPourUnGrandNombre() {
-		assertThat(fibo(1337)).isGreaterThan(0);
-	}
-
-	private int fibo(int nombre) {
-		if (nombre <= 1) {
-			return nombre;
-		}
-		return fib(nombre - 1, 0, 1);
-	}
-
-	private int fib(int nombre, int valeurN1, int valeurN) {
-		if (nombre == 0) {
-			return valeurN;
-		}
-		return fib(nombre - 1, valeurN, valeurN1 + valeurN);
+		assertThat(fibonacci.pour(1337)).isGreaterThan(0);
 	}
 }
